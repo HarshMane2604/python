249a0949ef79df88acc61400318c9fe10f38df4f
HarshMane2604 <maneharsh72@gmail.com>
2025-04-05 16:30:23 +0530

    Age group categorization

---
+++ 02_conditionals/01_ageGroupCategorization.py
@@ -0,0 +1,10 @@
+age = int(input("Please enter the age"))
+if age < 12:
+    print("You are a child")
+elif age >= 13 & age < 19:
+    print("You are a tennager")
+elif age >= 19 & age < 20:
+    print("You are Adult")
+else:
+    print("You are a tennager")
+
